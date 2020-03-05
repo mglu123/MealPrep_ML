b237960d4cc6c4c43f619e6750721065e606e6cf
Jarome Leslie <jsleslie@users.noreply.github.com>
2020-03-05 13:51:40 -0800

    assertion error tests + suggestion fix

--- tests/test_find_missing_ingredients.py
+++ tests/test_find_missing_ingredients.py
@@ -1,6 +1,7 @@
 from mealprep.mealprep import  find_missing_ingredients
 import pandas as pd
 import numpy as np
+import pytest
 
 
 def test_find_missing_ingredients():
@@ -10,6 +11,7 @@ def test_find_missing_ingredients():
     2. If the function returns the desired string if there are no missing values
     3. If the find_missing_ingredients can handle dataframe with NAs.
     4. If the find_missing_ingredients can handle empty dataframe.
+    5. If the find_missing_ingredients function confirms the right input type
     
    
     Example
@@ -27,3 +29,11 @@ def test_find_missing_ingredients():
     test2 = {'column1': [1,2,3], 'column2': [4,5,6]}
     df2 = pd.DataFrame(test2)
     assert(find_missing_ingredients(df2) == "There are no missing values")
+
+    test3 = {'column1': []}
+    df3 = pd.DataFrame(test3)
+    with pytest.raises(AssertionError):
+        find_missing_ingredients(df3)
+
+    with pytest.raises(AssertionError):
+        find_missing_ingredients("test")
