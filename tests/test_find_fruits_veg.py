9ebe96e70f48bae49fbeb6a776be3fa3d41ae635
mglu123 <46368106+mglu123@users.noreply.github.com>
2020-03-11 22:32:51 -0700

    Update test_find_fruits_veg.py

--- tests/test_find_fruits_veg.py
+++ tests/test_find_fruits_veg.py
@@ -30,8 +30,8 @@ def test_find_fruits_veg_empty():
     --------
     >>> test_find_fruits_veg()
     '''
-    df_few_nas=pd.DataFrame({'col1': [1, 2], 'col2': [np.nan, 'b']})
-    df_lot_nas=pd.DataFrame({'col1': [np.nan, 2], 'col2': ['a', np.nan]})
+    df_few_nas = pd.DataFrame({'col1': [1, 2], 'col2': [np.nan, 'b']})
+    df_lot_nas = pd.DataFrame({'col1': [np.nan, 2], 'col2': ['a', np.nan]})
     assert (find_fruits_veg(df_few_nas, type_of_out='categ') == [1])
     assert (find_fruits_veg(df_lot_nas, type_of_out='categ') ==
             "It is a empty data frame or too many missing data")
