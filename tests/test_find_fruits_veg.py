a3c4ab902c8bb69c0c305ac8c03f2ba49ff033e2
mglu123 <46368106+mglu123@users.noreply.github.com>
2020-03-11 22:21:47 -0700

    Update test_find_fruits_veg.py

--- tests/test_find_fruits_veg.py
+++ tests/test_find_fruits_veg.py
@@ -9,8 +9,6 @@ def test_find_fruits_veg():
     and it checks the following behaviour:
     1. If the find_fruits_veg finds numerical columns correctly
     2. If the find_fruits_veg finds categorical columns correctly
-
-
     Example
     --------
     >>> test_find_fruits_veg()
@@ -21,21 +19,19 @@ def test_find_fruits_veg():
     assert (find_fruits_veg(iris, type_of_out='num') == [0, 1, 2, 3])
     assert (find_fruits_veg(iris, type_of_out='categ') == [4])
 
+
 def test_find_fruits_veg_empty():
     '''
     This is a test function for find_fruits_veg,
     and it checks the following behaviour:
     1. If the find_fruits_veg can handle a data frame with NAs.
     2. If the find_fruits_veg can handle an empty data frame.
-
-
     Example
     --------
     >>> test_find_fruits_veg()
     '''
     iris = pd.read_csv(
-    df_few_nas = pd.DataFrame({'col1': [1, 2], 'col2': [np.nan, 'b']})
-    df_lot_nas = pd.DataFrame({'col1': [np.nan, 2], 'col2': ['a', np.nan]})
+    df_few_nas=pd.DataFrame({'col1': [1, 2], 'col2': [np.nan, 'b']})
+    df_lot_nas=pd.DataFrame({'col1': [np.nan, 2], 'col2': ['a', np.nan]})
     assert (find_fruits_veg(df_few_nas, type_of_out='categ') == [1])
     assert (find_fruits_veg(df_lot_nas, type_of_out='categ') ==
-            "It is a empty data frame or too many missing data")
