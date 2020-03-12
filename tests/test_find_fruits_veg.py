d541fd59605da1e24dfbdf7e7ec6b42fa694c913
luhuayue <luhuayueapp@163.com>
2020-03-11 21:42:57 -0700

    fix pep 8

--- tests/test_find_fruits_veg.py
+++ tests/test_find_fruits_veg.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 def test_find_fruits_veg():
     '''
-    This is a test function for find_fruits_veg, and it checks the following behaviour:
+    This is a test function for find_fruits_veg,
+    and it checks the following behaviour:
     1. If the find_fruits_veg finds numerical columns correctly
     2. If the find_fruits_veg finds categorical columns correctly
     3. If the find_fruits_veg can handle a data frame with NAs.
@@ -17,7 +18,8 @@ def test_find_fruits_veg():
     >>> test_find_fruits_veg()
     '''
     iris = pd.read_csv(
-        'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+        ('https://raw.githubusercontent.com/' +
+        'mwaskom/seaborn-data/master/iris.csv'))
     df_few_nas = pd.DataFrame({'col1': [1, 2], 'col2': [np.nan, 'b']})
     df_lot_nas = pd.DataFrame({'col1': [np.nan, 2], 'col2': ['a', np.nan]})
     assert (find_fruits_veg(iris, type_of_out='num') == [0, 1, 2, 3])
